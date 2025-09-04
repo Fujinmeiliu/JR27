33ab572e1dd9d78306c76a7636e4dbaddf99b578
Fujinmei Liu <jinmeiliufu125@gmail.com>
2025-09-04 13:39:04 +1000

    fixxed password-generator , it wasn't generate

--- Javascript/password-generator/script.js
+++ Javascript/password-generator/script.js
@@ -1,46 +1,45 @@
 function getRndomLower(){
     // ASCII a is 97
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97) 
 }
 
 function getRandomUpper(){
-    // ASCII A is 65
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    //ASCII A is 65
+    return String.fromCharCode( Math.floor(Math.random() * 26)  + 65)
 }
 
-function getRandomNumber () {
-    // 0-9 0 is 48 in Ascii 
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+function getRandomNumber(){
+    //  0-9
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)  
 }
 
-function getRandomSymbol (){
-    const symbols = '!@#$%^&*()_+?>';
-    // get symbols by symbols [] index
-    return symbols[Math.floor(Math.random() * symbols.length)]  
+function getRandomSymbol(){
+    const symbols = '!@#$%^&*()_+<>?/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)]  //0-symbols.length -1
 }
 const randomFunc = {
     lower: getRndomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
-    symbol:getRandomSymbol
+    symbol: getRandomSymbol
 }
-function generatePassword(lower, upper, number, symbol, length){
-    let generatedPassword='';
-    const typeCount = lower + upper + number + symbol
-    const typeArr = [{lower} , {upper}, {number}, {symbol}].filter(item=>Object.values(item)[0]);
-    if (typeCount === 0) {
+function generatePassword(lower,upper,number,symbol,length){
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol
+    const typesArr = [{lower},{upper},{number},{symbol}].filter(item=>Object.values(item)[0]);
+    // [{lower:true},{upper:true}]
+    if(typesCount === 0){
         return ''
     }
 
-    for (let i = 0; i < length; i += typeCount ) {
-        typeArr.forEach(type=>{
+    for(let i= 0; i < length; i += typesCount ){
+        typesArr.forEach(type=>{//type {lower:true} -> [lower]-> 'lower'
             const funcName = Object.keys(type)[0];
             generatedPassword += randomFunc[funcName]()
         })
     }
-    const finalPassword = generatedPassword.slice(0, length);
+    const finalPassword = generatedPassword.slice(0,length);
     return finalPassword;
-
 }
 
 const resultEl = document.getElementById('result');
@@ -52,21 +51,19 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
-generateEl.addEventListener('click', ()=>{
-    const length = Number(lengthEl.value);
+generateEl.addEventListener('click',()=>{
+    const length = Number(lengthEl.value) ;
     const hasLower = lowercaseEl.checked;
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numbersEl.checked;
     const hasSymbol = symbolsEl.checked;
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length )
-
+    resultEl.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol,length)
 })
 
-clipboardEl.addEventListener('click' ,()=> {
+clipboardEl.addEventListener('click',()=>{
     console.log('clipboard')
     const password = resultEl.innerText;
     if(!password) return;
     navigator.clipboard.writeText(password);
-    alert('Password Copied to clipboard')
-
+    alert('Password copied to clipboard!')
 })
